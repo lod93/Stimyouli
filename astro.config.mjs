17ff6323d0f1e9e03862c246fab6ed346e04c1a3
lod93 <problematicdelegate@gmail.com>
2026-03-29 06:53:38 +0200

    fix namespace imports,  better styling

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,6 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import react from '@astrojs/react';
-
 export default defineConfig({
-  site: 'https://stimyouli.github.io',
-  base: '/stimyouli-public-repo',
+  site: 'https://lod93.github.io',
+  base: '/Stimyouli',
   integrations: [tailwind(), react()],
   output: 'static'
 });
