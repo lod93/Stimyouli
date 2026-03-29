13d20848975d91a15022b1199b96d60822f8cf95
lod93 <problematicdelegate@gmail.com>
2026-03-29 08:39:14 +0200

    feat: initialize project structure with core pages, layout, and components

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://lod93.github.io',
+  site: 'https://lod93.github.io/Stimyouli/',
   base: '/Stimyouli',
   integrations: [tailwind(), react()],
   output: 'static'
